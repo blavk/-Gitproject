1545c1eaba150bcb2de55ac4521d799bac805d20
937826476@qq.com <dabaitunaitang>
2017-07-05 12:17:25 +0800

    dbt

--- src/com/miji/Newjianfa.java
+++ src/com/miji/Newjianfa.java
@@ -1,10 +1,10 @@
-package src.com.miji;
+package com.miji;
 
 public class Newjianfa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("new jianfa");
+		System.out.println("new jianfa22");
 
 	}
 
